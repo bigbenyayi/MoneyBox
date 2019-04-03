c9bbad8ee18d242b2687c431774bfc24dc64da5a
Benjamin Corben <corbenbenjamin@gmail.com>
2019-04-03 22:25:19 +0200

    Bug 3 fixed

---
+++ app/src/main/java/com/example/minimoneybox/LoginActivityJava.java
@@ -0,0 +1,114 @@
+package com.example.minimoneybox;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.os.Handler;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.airbnb.lottie.LottieAnimationView;
+
+import java.util.regex.Pattern;
+
+public class LoginActivityJava extends AppCompatActivity {
+
+    EditText emailEditText;
+    EditText passwordEditText;
+    EditText nameEditText;
+    Button signInButton;
+    LottieAnimationView pigAnimation;
+    TextInputLayout emailTIL;
+    TextInputLayout passwordTIL;
+    TextInputLayout nameTIL;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_java);
+        setupViews();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        pigAnimation.setMinAndMaxFrame(0, 109);
+        pigAnimation.playAnimation();
+
+        pigAnimation.addAnimatorListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                pigAnimation.setMinAndMaxFrame(131, 158);
+                pigAnimation.loop(true);
+                pigAnimation.playAnimation();
+            }
+        });
+    }
+
+    private void setupViews() {
+        emailEditText = findViewById(R.id.et_email);
+        passwordEditText = findViewById(R.id.et_password);
+        nameEditText = findViewById(R.id.et_name);
+        signInButton = findViewById(R.id.btn_sign_in);
+        pigAnimation = findViewById(R.id.animation);
+        emailTIL = findViewById(R.id.til_email);
+        passwordTIL = findViewById(R.id.til_password);
+        nameTIL = findViewById(R.id.til_name);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (allFieldsValid()) {
+                    Toast.makeText(LoginActivityJava.this, getString(R.string.input_valid), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private boolean allFieldsValid() {
+        boolean allValid = false;
+
+        String EMAIL_REGEX = "[^@]+@[^.]+\\..+";
+        String NAME_REGEX = "[a-zA-Z]{6,30}";
+        String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[A-Z]).{10,50}$";
+
+        if (Pattern.matches(EMAIL_REGEX, emailEditText.getText().toString())) {
+            allValid = true;
+            emailTIL.setError("");
+            emailTIL.setHint("Email");
+        } else {
+            emailTIL.setError(getString(R.string.prompt_email));
+            emailTIL.setHint("");
+        }
+
+        if (Pattern.matches(PASSWORD_REGEX, passwordEditText.getText().toString())) {
+            allValid = true;
+            passwordTIL.setError("");
+            passwordTIL.setHint(getString(R.string.prompt_password));
+        } else {
+            passwordTIL.setError(getString(R.string.password_error));
+            passwordTIL.setHint("");
+        }
+
+        if (Pattern.matches(NAME_REGEX, nameEditText.getText().toString()) || nameEditText.getText().toString().equals("")) {
+            allValid = true;
+            nameTIL.setError("");
+            nameTIL.setHint(getString(R.string.prompt_full_name));
+        } else {
+            nameTIL.setError(getString(R.string.full_name_error));
+            nameTIL.setHint("");
+        }
+        return allValid;
+    }
+
+//        val firstAnim = 0 to 109;
+//        val secondAnim = 131 to 158;
+
+}
